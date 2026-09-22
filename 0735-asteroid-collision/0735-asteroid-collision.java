286d8fb2aba19690163827e3b30bd109f5b4a927
AVinod6 <vtu28340@veltech.edu.in>
2026-09-22 12:14:36 +0530

    Time: 2 ms (97.17%), Space: 46.7 MB (89.21%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int asteroid : asteroids) {
+            boolean exploded = false;
+
+            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                }
+                exploded = true;
+                break;
+            }
+
+            if (!exploded) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
